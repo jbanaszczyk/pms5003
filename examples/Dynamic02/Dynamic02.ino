e2752d21057c4017b631d203ea3b3661416dd806
Jacek Banaszczyk <jacek.banaszczyk@gmail.com>
2018-04-19 22:15:39 +0200

    operator [] in lerge scale

--- examples/Dynamic02/Dynamic02.ino
+++ examples/Dynamic02/Dynamic02.ino
@@ -10,32 +10,35 @@
 PmsAltSerial pmsSerial;
 
 #if defined PMS_DYNAMIC
-Pms *pms_ = nullptr;
+Pms* pms_ = nullptr;
 #define pms (*pms_)
 #else
 Pms pms(&pmsSerial);
 #endif
 
+// Green 8
+// Blue  9
+// Brown GND
+// Black VCC
+
 ////////////////////////////////////////
 
-// ReSharper disable CppInconsistentNaming
+// ReSharper disable once CppInconsistentNaming
 void setup(void) {
-	// ReSharper restore CppInconsistentNaming
-	Serial.begin(115200);
-	while (!Serial) {}
-	Serial.println("PMS5003");
+    Serial.begin(115200);
+    while (!Serial) {}
+    Serial.println("PMS5003");
 
+    #if defined PMS_DYNAMIC
+    pms_ = new Pms(&pmsSerial);
+    #else
+    // ReSharper disable once CppExpressionWithoutSideEffects
+    pms.begin();
+    #endif
 
-#if defined PMS_DYNAMIC
-	pms_ = new Pms(&pmsSerial);
-#else
-	// ReSharper disable once CppExpressionWithoutSideEffects
-	pms.begin();
-#endif
-
-	pms.write(Pms::PmsCmd::CMD_WAKEUP);
-	pms.write(Pms::PmsCmd::CMD_MODE_ACTIVE);
-	pms.waitForData(Pms::WAKEUP_TIME);
+    pms.write(Pms::PmsCmd::CMD_WAKEUP);
+    pms.write(Pms::PmsCmd::CMD_MODE_ACTIVE);
+    pms.waitForData(Pms::WAKEUP_TIME);
 }
 
 ////////////////////////////////////////
@@ -43,37 +46,37 @@ void setup(void) {
 // ReSharper disable once CppInconsistentNaming
 void loop(void) {
 
-	static auto lastRead = millis();
+    static auto lastRead = millis();
+    Pms::PmsData data;
+    auto status = pms.read(data);
 
-	Pms::PmsData data;
-	auto status = pms.read(data);
+    switch (status) {
+        case Pms::PmsStatus::OK: {
+            Serial.println("_________________");
+            const auto newRead = millis();
+            Serial.print("Wait time ");
+            Serial.println(newRead - lastRead);
+            lastRead = newRead;
 
-	switch (status) {
-	case Pms::PmsStatus::OK: {
-		Serial.println("_________________");
-		const auto newRead = millis();
-		Serial.print("Wait time ");
-		Serial.println(newRead - lastRead);
-		lastRead = newRead;
-
-		auto view = data.particles;
-		for (auto i = 0; i < view.getSize(); ++i) {
-			Serial.print(view[i]);
-			Serial.print("\t");
-			Serial.print(view.getName(i));
-
-			Serial.print(" [");
-			Serial.print(view.getMetric(i));
-			Serial.print("] ");
-			Serial.print(view.getLevel(i));
-			Serial.println();
-		}
-		break;
-	}
-	case Pms::PmsStatus::NO_DATA:
-		break;
-	default:
-		Serial.print("!!! Pms error: ");
-		Serial.println(status.getErrorMsg());
-	}
+            auto view = data.particles;
+            for (auto i = 0; i < view.SIZE; ++i) {
+                Serial.print(view[i]);
+                Serial.print("\t");
+                Serial.print(view.names[i]);
+                Serial.print("\t");
+                Serial.print(view.diameters[i]);
+                Serial.print(" [");
+                Serial.print(view.metrics[i]);
+                Serial.print("] ");
+                Serial.print(view.getLevel(i));
+                Serial.println();
+            }
+            break;
+        }
+        case Pms::PmsStatus::NO_DATA:
+            break;
+        default:
+            Serial.print("!!! Pms error: ");
+            Serial.println(status.getErrorMsg());
+    }
 }
--- examples/Simple01/Simple01.ino
+++ examples/Simple01/Simple01.ino
@@ -14,14 +14,14 @@ Pms pms(&pmsSerial);
 
 // ReSharper disable once CppInconsistentNaming
 void setup(void) {
-	Serial.begin(115200);
-	while (!Serial) {}
-	Serial.println("PMS5003");
+    Serial.begin(115200);
+    while (!Serial) {}
+    Serial.println("PMS5003");
 
-	pms.begin();
-	pms.write(Pms::PmsCmd::CMD_WAKEUP);
-	pms.write(Pms::PmsCmd::CMD_MODE_ACTIVE);
-	pms.waitForData(Pms::WAKEUP_TIME);
+    pms.begin();
+    pms.write(Pms::PmsCmd::CMD_WAKEUP);
+    pms.write(Pms::PmsCmd::CMD_MODE_ACTIVE);
+    pms.waitForData(Pms::WAKEUP_TIME);
 }
 
 ////////////////////////////////////////
@@ -29,37 +29,37 @@ void setup(void) {
 // ReSharper disable once CppInconsistentNaming
 void loop(void) {
 
-	static auto lastRead = millis();
+    static auto lastRead = millis();
 
-	Pms::PmsData data;
-	auto status = pms.read(data);
+    Pms::PmsData data;
+    auto status = pms.read(data);
 
-	switch (status) {
-	case Pms::PmsStatus::OK: {
-		Serial.println("_________________");
-		const auto newRead = millis();
-		Serial.print("Wait time ");
-		Serial.println(newRead - lastRead);
-		lastRead = newRead;
+    switch (status) {
+        case Pms::PmsStatus::OK: {
+            Serial.println("_________________");
+            const auto newRead = millis();
+            Serial.print("Wait time ");
+            Serial.println(newRead - lastRead);
+            lastRead = newRead;
 
-		auto view = data.particles;
-		for (auto i = 0; i < view.getSize(); ++i) {
-			Serial.print(view[i]);
-			Serial.print("\t");
-			Serial.print(view.getName(i));
+            auto view   = data.particles;
+            for (auto i = 0; i < view.getSize(); ++i) {
+                Serial.print(view[i]);
+                Serial.print("\t");
+                Serial.print(view.getName(i));
 
-			Serial.print(" [");
-			Serial.print(view.getMetric(i));
-			Serial.print("] ");
-			Serial.print(view.getLevel(i));
-			Serial.println();
-		}
-		break;
-	}
-	case Pms::PmsStatus::NO_DATA:
-		break;
-	default:
-		Serial.print("!!! Pms error: ");
-		Serial.println(status.getErrorMsg());
-	}
+                Serial.print(" [");
+                Serial.print(view.getMetric(i));
+                Serial.print("] ");
+                Serial.print(view.getLevel(i));
+                Serial.println();
+            }
+            break;
+        }
+        case Pms::PmsStatus::NO_DATA:
+            break;
+        default:
+            Serial.print("!!! Pms error: ");
+            Serial.println(status.getErrorMsg());
+    }
 }
