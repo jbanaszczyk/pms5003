73178a81ef73ad8e5dccebf2cb99f82d7fbbd91d
Jacek Banaszczyk <jacek.banaszczyk@gmail.com>
2018-05-03 21:24:21 +0200

    Examples p04usingPmsx added

---
+++ Examples/p04usingPmsx/p04usingPmsx.ino
@@ -0,0 +1,95 @@
+#include <pms.h>
+
+using namespace pmsx;
+
+PmsAltSerial pmsSerial;
+Pms pms(&pmsSerial);
+
+////////////////////////////////////////
+
+// * PMS5003 Pin 1 (black) : VCC +5V
+// * PMS5003 Pin 2 (brown) : GND
+// Important: pms5003 uses 3.3V logic.Use converters if required or make sure your Arduino board uses 3.3V logic too.
+// * PMS5003 Pin 4 (blue) : Digital pin 9 (there is no choice, forced by AltSerial)
+// * PMS5003 Pin 5 (green) : Digital pin 8 (there is no choice, forced by AltSerial)
+// * Optional
+//   * PMS5003 Pin 3 (white) : Digital pin 7 (can be changed or not connected at all)
+//   * PMS5003 Pin 6 (violet) : Digital pin 6 (can be changed or not connected at all)
+
+////////////////////////////////////////
+
+void setup(void) {
+    Serial.begin(115200);
+    while (!Serial) {}
+    Serial.println("PMS5003");
+
+    if (!pms.begin()) {
+        Serial.println("PMS sensor: communication failed");
+        return;
+    }
+
+    pms.setPinReset(6);
+    pms.setPinSleepMode(7);
+
+    if (!pms.write(PmsCmd::CMD_RESET)) {
+        pms.write(PmsCmd::CMD_SLEEP);
+        pms.write(PmsCmd::CMD_WAKEUP);
+    }
+    pms.write(PmsCmd::CMD_MODE_PASSIVE);
+    pms.write(PmsCmd::CMD_READ_DATA);
+    pms.waitForData(Pms::TIMEOUT_PASSIVE, PmsData::FRAME_SIZE);
+    PmsData data;
+    auto status = pms.read(data);
+    if (status != PmsStatus::OK) {
+        Serial.print("PMS sensor: ");
+        Serial.println(status.getErrorMsg());
+    }
+    pms.write(PmsCmd::CMD_MODE_ACTIVE);
+    if (!pms.isWorking()) {
+        Serial.println("PMS sensor failed");
+    }
+
+    Serial.print("Time of setup(): ");
+    Serial.println(millis());
+}
+
+////////////////////////////////////////
+
+void loop(void) {
+
+    static auto lastRead = millis();
+
+    PmsData data;
+    auto status = pms.read(data);
+
+    switch (status) {
+    case PmsStatus::OK: {
+        Serial.println("_________________");
+        const auto newRead = millis();
+        Serial.print("Wait time ");
+        Serial.println(newRead - lastRead);
+        lastRead = newRead;
+
+        auto view = data.particles;
+        for (auto i = 0; i < view.getSize(); ++i) {
+            Serial.print(view.getValue(i));
+            Serial.print("\t");
+            Serial.print(view.getName(i));
+            Serial.print(" [");
+            Serial.print(view.getMetric(i));
+            Serial.print("] ");
+            Serial.print(" Level: ");
+            Serial.print(view.getLevel(i));
+            Serial.print(" | diameter: ");
+            Serial.print(view.getDiameter(i));
+            Serial.println();
+        }
+        break;
+    }
+    case PmsStatus::NO_DATA:
+        break;
+    default:
+        Serial.print("!!! Pms error: ");
+        Serial.println(status.getErrorMsg());
+    }
+}
